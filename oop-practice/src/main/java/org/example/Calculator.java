4dacd75ce8daede09dbc28d20ee036014eec4545
Youngju-Jang <joj1043@github.com>
2022-10-06 11:53:17 +0900

    enum values

---
+++ oop-practice/src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        ArithmeticOperator.caculate(operand1, operator, operand2);
+        return 0;
+    }
+}
